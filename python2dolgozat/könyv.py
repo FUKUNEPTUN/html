2eb6821b4b443ffeb0f3a9e83c8788d6bb6d5536
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2022-01-07 14:09:58 +0100

    Update könyv.py

--- python2dolgozat/könyv.py
+++ python2dolgozat/könyv.py
@@ -1,10 +1,10 @@
-a = input("add meg a könyvnek a címét, entert ha kilépnél             ")
+a = input("Add meg a könyvnek a címét, entert ha kilépnél   ")
 def kalapacs(b):
     if b >= 150:
-        print("A", a, "könyv hosszú terjedelmű")
+        print("A(z)", a, "könyv hosszú terjedelmű")
     else:
-        print("A ",a , " könyved rövid")
+        print("A(z) ",a , " könyved rövid")
 while a != "":
-    kalapacs(int(input("add meg az oldalak számát         ")))
-    a = input("add meg a könyvnek a címét, entert ha kilépnél             ")
+    kalapacs(int(input("add meg az oldalak számát   ")))
+    a = input("add meg a könyvnek a címét, entert ha kilépnél   ")
 print("A programból sikeresen kiléptél")
